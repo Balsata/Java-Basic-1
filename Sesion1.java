4144607579d922c61dfc4c8f26bd6d18fa3d761c
Balsata <109769483+Balsata@users.noreply.github.com>
2022-08-10 15:43:12 -0600

    Add files via upload

---
+++ Sesion1.java
@@ -0,0 +1,33 @@
+package com.example;
+
+public class Sesion1 {
+
+    public static void main(String[] args) {
+
+        //Numericos
+        int num1 = 1;
+        System.out.println(num1);
+
+        float num2 = 2.4123f;
+        System.out.println(num2);
+
+        double num3 = 3.3d;
+        System.out.println(num3);
+
+        //Boolean
+        boolean bool1 = true;
+        System.out.println(bool1);
+
+        boolean bool2 = false;
+         System.out.println(bool2);
+
+        //Text
+        String text1 = "Hello World!";
+        System.out.println(text1);
+
+        char text2 = 'A';
+        System.out.println(text2);
+
+    }
+
+}
